88269e075bd6092ed2952de8fc27843601a77075
chloekim917 <chloekim917@gmail.com>
2023-04-18 16:38:53 +0900

    solved str prob

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 print('Welcome to the Guessing Game! Please input your range.')
+count = 0
 begin = int(input('From: '))
 end = int(input('To: '))
 yorno = input(f"So you want to guess a number between {begin} and {end}? Write 'yes' or 'no': ")
 if yorno.upper() == "YES":
     import random
     thenumb = random.randint(begin, end)
-    print (thenumb)
+    print(thenumb)
+    i = int(input("Guess a number: "))
 
-    """
-    print("Guess a number: ")
-    i = input("Alright then, guess a number: ")
     if i < thenumb:
-        input("Higher! Guess a new number: ")
+        count+1
+        i = int(input("Higher! Guess a new number: "))
     elif i > thenumb:
-        input ("Lower! Guess a new number: ")
-    
-    """
-
-     
+        count+1
+        i = int(input("Lower! Guess a new number: "))
+    else:
+        print(f"Congrats! You got it right! It took you {i} tries")
